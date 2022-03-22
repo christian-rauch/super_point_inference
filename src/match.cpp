1c3abae37639b215f22b2539e1bf4031ff4d1e60
Christian Rauch <Christian.Rauch@ed.ac.uk>
2022-03-22 20:10:40 +0000

    draw & export tracks

--- src/match.cpp
+++ src/match.cpp
@@ -2,8 +2,10 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <random>
 #include <super_point_inference.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/viz/types.hpp>
 #include <opencv2/core/eigen.hpp>
 #include <experimental/filesystem>
 
@@ -62,6 +64,38 @@ draw_matches(const Eigen::MatrixXf &last_keypoints,
   return img_matches;
 }
 
+cv::Mat
+draw_tracks(const Eigen::MatrixXf &last_keypoints,
+            const Eigen::MatrixXf &next_keypoints,
+            const std::vector<std::tuple<int, int, float>> &correspondences,
+            const cv::Mat &next_img)
+{
+  std::vector<cv::Point> last_kp(last_keypoints.rows());
+  for(size_t i=0; i<last_kp.size(); i++)
+    last_kp[i] = cv::Point(last_keypoints.row(i)[0]*next_img.cols, last_keypoints.row(i)[1]*next_img.rows);
+
+  std::vector<cv::Point> next_kp(next_keypoints.rows());
+  for(size_t i=0; i<next_kp.size(); i++)
+    next_kp[i] = cv::Point(next_keypoints.row(i)[0]*next_img.cols, next_keypoints.row(i)[1]*next_img.rows);
+
+  cv::Mat img_tracks;
+  cv::cvtColor(next_img, img_tracks, cv::COLOR_RGB2GRAY);
+  cv::cvtColor(img_tracks, img_tracks, cv::COLOR_GRAY2RGB);
+
+  std::uniform_real_distribution<double> u(0,1);
+  std::default_random_engine g;
+
+  for (const auto &[last_id, next_id, distance]: correspondences) {
+    // sample random colour with track-specific seed
+    g.seed(last_id);
+    const cv::viz::Color c(u(g)*255, u(g)*255, u(g)*255);
+    // draw track segments
+    cv::line(img_tracks, last_kp[last_id], next_kp[next_id], c, 2);
+  }
+
+  return img_tracks;
+}
+
 int main(int argc, char *argv[]) {
   if (argc<4) {
     std::cerr << "wrong set of arguments, expected:" << std::endl << argv[0] << " <model_path> <image_1_path> ... <image_N_path>" << std::endl;
@@ -91,12 +125,17 @@ int main(int argc, char *argv[]) {
     cv::cvtColor(img[i], rgb, cv::COLOR_BGR2RGB);
     std::tie(feat[i], coord[i], descr[i]) = sp->getFeatures(rgb);
     if (i>0) {
+      // draw & export matches
       const auto matches = pairwise_matches(descr[i-1].cast<float>(), descr[i].cast<float>());
       const cv::Mat img_matches = draw_matches(coord[i-1].cast<float>(), coord[i].cast<float>(), matches, img[i-1], img[i]);
-      // export matches
-      const std::string match_name = "matches "+std::to_string(i-1)+"-"+std::to_string(i);
-      cv::imshow(match_name, img_matches);
-      cv::imwrite(fs::temp_directory_path() / fs::path(match_name+".png"), img_matches);
+      const std::string match_name = std::to_string(i-1)+"-"+std::to_string(i);
+      cv::imshow("matches "+match_name, img_matches);
+      cv::imwrite(fs::temp_directory_path() / fs::path("matches-"+match_name+".png"), img_matches);
+
+      // draw & export tracks
+      const cv::Mat img_tracks = draw_tracks(coord[i-1].cast<float>(), coord[i].cast<float>(), matches, img[i]);
+      cv::imshow("tracks "+match_name, img_tracks);
+      cv::imwrite(fs::temp_directory_path() / fs::path("tracks-"+match_name+".png"), img_tracks);
     }
   }
   cv::waitKey();
